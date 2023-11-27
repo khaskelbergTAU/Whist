8f2fd395dc7ee104678abf85adce73fb87e1869e
omrisdeor <omrisdeor@mail.tau.ac.il>
2023-11-27 13:45:47 +0200

    added betting rounds

--- API/grader.cpp
+++ API/grader.cpp
@@ -1,15 +1,73 @@
 #include <stdlib.h>
+#include <bits/stdc++.h>
 
 #include "whist.h"
 
-#define ROUNDS 13
-
-void main_bets() {
-    
+int compare_bets(bet_t bet1, bet_t bet2) {
+    if(bet1.number > bet2.number) {
+        return 1;
+    }
+    if(bet1.number < bet2.number) {
+        return -1;
+    }
+    if(bet1.suit < bet2.suit) {
+        return 1;
+    }
+    if(bet1.suit > bet2.suit) {
+        return -1;
+    }
+    return 0;
 }
 
-void second_bets() {
+void shuffle_cards(card_t cards[4][13]) {
+    card_t all_cards[4 * 13];
+    for(int i = 0; i < 4 * 13; i++) {
+        all_cards[i].suit = (suit_e) ((i / 13) + 1);
+        all_cards[i].number = 2 + (i % 13);
+    }
+    std::shuffle(all_cards, all_cards + 4 * 13, std::default_random_engine(0));
+    for(int i = 0; i < 4 * 13; i++) {
+        cards[i / 13][i % 13] = all_cards[i];
+    }
+}
 
+std::pair<bet_t, size_t> main_bets(card_t cards[4][13]) {
+    int last_changed = 0;
+    int player = 0;
+    bet_t best_bet = {CLUBS, 0};
+    bets_t bets;
+    for(int i = 0; i < 4; i++) {
+        bets.cards[i] = {CLUBS, 0};
+    }
+    while(last_changed < 4) {
+        bet_t bet = place_initial_bet(player, cards[player], bets);
+        if(compare_bets(bet, BET_PASS) != 0 && (bet.number < 4 || bet.number > 13 || compare_bets(bet, best_bet) <= 0)) {
+            // replace player with bot
+            bet_t bet = place_initial_bet(player, cards[player], bets);
+        }
+        if(compare_bets(bet, best_bet) > 0) {
+            best_bet = bet;
+            last_changed = 0;
+        }
+        bets.cards[player] = bet;
+        last_changed++;
+        player = (player + 1) % 4;
+    }
+    return std::pair<bet_t, size_t> (best_bet, player);
+}
+
+void final_bets(bet_t highest_bet, size_t highest_bidder, size_t final_bets[4]) {
+    for(int i = 0; i < 4; i++) {
+        final_bets[i] = 0;
+    }
+    for(int i = 0; i < 4; i++) {
+        size_t final_bet = place_final_bet(highest_bet.suit, highest_bidder, final_bets);
+        if(final_bet > 13 || (i == 0 && final_bet < highest_bet.number) || (i == 3 && final_bets[0] + final_bets[1] + final_bets[2] + final_bets[3] + final_bet == 13)) {
+            // replace player with bot
+            final_bet = place_final_bet(highest_bet.suit, highest_bidder, final_bets);
+        }
+        final_bets[(highest_bidder + i) % 4] = final_bet;
+    }
 }
 
 void play_round() {
@@ -22,10 +80,15 @@ void get_results() {
 
 int main(int argc, char * argv[]) {
     int games = atoi(argv[1]);
-    for(int game = 0; game < games; i++) {
-        main_bets();
-        second_bets();
-        for(int round = 0; round < ROUNDS; round++) {
+    for(int game = 0; game < games; game++) {
+        card_t cards[4][13];
+        shuffle_cards(cards);
+        std::pair<bet_t, size_t> bet_data = main_bets(cards);
+        suit_e trump = bet_data.first.suit;
+        size_t highest_bidder = bet_data.second;
+        size_t bets[4];
+        final_bets(bet_data.first, highest_bidder, bets);
+        for(int round = 0; round < 13; round++) {
             play_round();
         }
         get_results();
--- API/whist.h
+++ API/whist.h
@@ -1,11 +1,11 @@
 #ifndef WHIST_H
 #define WHIST_H
 typedef enum suit {
-	HEARTS,
-	CLUBS,
-	DIAMONDS,
-	SPADES,
 	NONE,
+	SPADES,
+	HEARTS,
+	DIAMONDS,
+	CLUBS,
 } suit_e;
 
 typedef struct card {
@@ -14,16 +14,17 @@ typedef struct card {
 } card_t, bet_t;
 
 typedef struct round {
-	size_t first_player;
 	card_t cards[4];
 } round_t, bets_t;
+
+const bet_t BET_PASS = {NONE, 0};
 
 /* to be defined by player: */
 bet_t place_initial_bet(size_t player_position, card_t my_hand[13], bets_t previous_bets);
 
-size_t place_final_bet(bets_t initial_bets, size_t final_bets[4]);
+size_t place_final_bet(suit_e trump, size_t highest_bidder, size_t final_bets[4]);
 
-card_t play_card_phase(round_t previous_round, round_t current_round);
+card_t play_card(round_t previous_round, round_t current_round);
 
 void game_over(round_t final_round);
 #endif
