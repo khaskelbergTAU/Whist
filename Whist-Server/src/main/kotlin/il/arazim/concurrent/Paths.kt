15d5f1794e3847653884ae048e6238adc53f2693
Mark <34838287+markK24@users.noreply.github.com>
2023-11-29 05:01:31 +0200

    Fixed weird imports in Paths.kt and Uploader.kt

--- Whist-Server/src/main/kotlin/il/arazim/concurrent/Paths.kt
+++ Whist-Server/src/main/kotlin/il/arazim/concurrent/Paths.kt
@@ -8,10 +8,10 @@ import kotlin.io.path.createDirectories
 
 
 fun getSubmissionDir(group: String): Path = rootDir.resolve("bots/$group/submissions").apply { createDirectories() }
-fun getSourceDir(group: String): Path = il.arazim.il.arazim.getSubmissionDir(group).resolve("source").apply { createDirectories() }
-fun getCompilationLogsDir(group: String): Path = il.arazim.il.arazim.getSubmissionDir(group).resolve("clogs").apply { createDirectories() }
-fun getExecutablesDir(group: String): Path = il.arazim.il.arazim.getSubmissionDir(group).resolve("exec").apply { createDirectories() }
-fun getLatest(group: String): Path = il.arazim.il.arazim.getSubmissionDir(group).resolve("latest.txt")
+fun getSourceDir(group: String): Path = getSubmissionDir(group).resolve("source").apply { createDirectories() }
+fun getCompilationLogsDir(group: String): Path = getSubmissionDir(group).resolve("clogs").apply { createDirectories() }
+fun getExecutablesDir(group: String): Path = getSubmissionDir(group).resolve("exec").apply { createDirectories() }
+fun getLatest(group: String): Path = getSubmissionDir(group).resolve("latest.txt")
 
 
 fun getWrapper() = Path("./API/API.c")
